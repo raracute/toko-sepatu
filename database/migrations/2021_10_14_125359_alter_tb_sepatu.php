22df9130f928525eb43b437146f767f971c365cb
raracute <ptrirds068@gmail.com>
2021-10-15 17:14:07 +0700

    fix: composer

--- database/migrations/2021_10_14_125359_alter_tb_sepatu.php
+++ database/migrations/2021_10_14_125359_alter_tb_sepatu.php
@@ -14,42 +14,8 @@ class AlterTbSepatu extends Migration
     public function up()
     {
         Schema::table('tb_sepatu', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->integer('harga')->change();
             $table->integer('jumlah_stok')->change();
-=======
-            $table->dropColumn('harga');
-            $table->dropColumn('jumlah_Stok');
         });
-
         Schema::table('tb_sepatu', function (Blueprint $table) {
-            $table->integer('harga');
-            $table->integer('jumlah_Stok');
->>>>>>> a7d93853c48704f2d664adf622b24a42a4684f7f
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('tb_sepatu', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->string('harga')->change();
             $table->string('jumlah_stok')->change();
-=======
-            $table->dropColumn('harga');
-            $table->dropColumn('jumlah_Stok');
         });
-
-
-        Schema::table('tb_sepatu', function (Blueprint $table) {
-            $table->string('harga');
-            $table->string('jumlah_Stok');
->>>>>>> a7d93853c48704f2d664adf622b24a42a4684f7f
-        });
-    }
-}
